4ff61eae96418e833b586559796af48e705c852b
bobo <q463250938@gmail.com>
2023-10-30 12:16:28 -0700

    video processed locally with ffmpeg

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,13 +1,33 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const PORT = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello, World!");
+
+app.post('/process-video', (req, res) => {
+    // Get path of the input video file from the request body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad Request Missing File Path.");
+    }
+
+    ffmpeg(inputFilePath) 
+        .outputOptions("-vf", "scale=-1:360")
+        .on("end", () => {
+            res.status(200).send("Processing finished successfully.")
+        })
+        .on("error", (err) => {
+            console.log(`An error occured: ${err.message}`)
+            res.status(500).send(`Internal Server Error: ${err.message}`)
+        })
+        .save(outputFilePath);
 });
 
-app.listen(PORT, ()=> {
-    console.log(`Server running at http://localhost:${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
 
